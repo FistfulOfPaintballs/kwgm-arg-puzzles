871236f6b8b42f216cf0f1dc83a01a0e22b1f2b1
FistfulOfPaintballs <erinbleiweiss+fistfulofpaintballs@gmail.com>
2023-05-18 00:26:05 -0500

    test for github pages

--- static/js/main.js
+++ static/js/main.js
@@ -89,7 +89,7 @@ var selectionRectangle = new Konva.Rect({
 });
 layer.add(selectionRectangle);
 
-fetch('./static/img/_pieces.json')
+fetch('../img/_pieces.json')
     .then((response) => response.json())
     .then((json) => {
         let pieces = json["pieces"];
